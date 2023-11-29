414b0fc9fcb05b981840419d2e00ca02286549d4
Kalpesh Dev <kalpesh.maru@codeb.co.in>
2023-11-29 11:52:56 +0530

    Added Tailwind CSS

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,14 @@
+import "./login.scss";
+
+function Login() {
+  return (
+    <div className="container">
+      <div className="left">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+}
+
+export default Login;
